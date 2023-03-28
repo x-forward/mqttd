018081b24fe68bb5b46ebef48c088eace5f50bd3
laputa <laputa@localhost.localdomain>
2023-03-28 20:25:17 +0800

    init

--- broker/src/server.rs
+++ broker/src/server.rs
@@ -1,1 +1,10 @@
+use config::server::Capability;
+use network::tcp;
 
+pub struct Option {
+    pub server_cap: Capability,
+}
+
+pub struct Broker {}
+
+pub async fn run() {}
--- network/src/lib.rs
+++ network/src/lib.rs
@@ -1,4 +1,13 @@
-pub mod codec;
-pub mod connection;
-pub mod shutdown;
 pub mod tcp;
+
+#[cfg(test)]
+mod tests {
+    use crate::tcp::listener;
+    use tokio::net::TcpListener;
+    #[tokio::test]
+    async fn it_works() {
+        tracing_subscriber::fmt::init();
+        let listener = TcpListener::bind("127.0.0.1:1883").await.unwrap();
+        listener::run(listener, tokio::signal::ctrl_c()).await;
+    }
+}
