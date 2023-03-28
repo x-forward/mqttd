4ab0de314e045c589a5518c1a6b37d76bac652a3
laputa <laputa@localhost.localdomain>
2023-03-28 17:04:57 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,2 @@
-use broker::server::Server;
-use config::Config;
-
 #[tokio::main]
-async fn main() {
-    let s = Server::new(Config::default());
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+async fn main() {}
